94dc00bb325b22a8047680435d1ca05935b9f124
Ankit Prakash <ankit.prakash687@gmail.com>
2020-10-05 08:06:50 -0400

    Update app bar alignment

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -1,22 +1,32 @@
 <template>
-  <div app color="primary" dark height="100" class="ma-0 appbar">
-    <v-container>
+  <div
+    app
+    color="primary"
+    dark
+    height="100"
+    class="ma-0 bg-primary bottom-left-rounded-sm"
+  >
+    <v-container class="app-con">
       <v-row>
-        <div class="d-flex">
-          <div class="text-weight-bold text-h6">devJobs</div>
-        </div>
-
+        <v-col class="mt-2">
+          <v-row justify="center">
+            <div class="text-weight-bold text-h6">devJobs</div>
+          </v-row>
+        </v-col>
         <v-spacer></v-spacer>
-
-        <v-switch
-          :input-value="darkMode"
-          inset
-          :ripple="false"
-          color="accent"
-          dense
-          @change="toggleTheme()"
-        >
-        </v-switch>
+        <v-col>
+          <v-row justify="center">
+            <v-switch
+              :input-value="darkMode"
+              inset
+              :ripple="false"
+              color="accent"
+              dense
+              @change="toggleTheme()"
+            >
+            </v-switch>
+          </v-row>
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -38,8 +48,16 @@ export default {
 </script>
 
 <style>
-.appbar {
+.bg-primary {
   background-color: var(--v-primary-base);
-  border-bottom-left-radius: 60px;
+}
+
+.app-con {
+  max-width: 1200px !important;
+}
+@media only screen and (min-width: 600px) {
+  .bottom-left-rounded-sm {
+    border-bottom-left-radius: 60px;
+  }
 }
 </style>
